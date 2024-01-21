6d4aba9cc7fbfbcf632d60c7f5c6d1dcb8eff348
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2024-01-21 18:56:35 +0000

    OpenAI API get endpoint added

--- server.js
+++ server.js
@@ -1,69 +1,61 @@
 // server.js
-import express from 'express';
-import fetch from 'node-fetch';
-import dotenv from 'dotenv';
-// import { Loader } from "@googlemaps/js-api-loader"
+import express from "express";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+import OpenAI from "openai";
 
 export const app = express();
-
 dotenv.config();
-
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+  console.log("Server is running on http://localhost:3000");
 });
 
-// const lat = 51.5072;
-// const lon = 0.1276;
-// const part = '';
 const weatherAPI = process.env.openweathermapAPI;
 const googleAPI = process.env.googleAPI;
+const openaiAPI = process.env.openaiAPI;
+let globalPostcode = "";
 
 // JSON Endpoints made with Express
 
-app.get('/weather', async (req, res) => {
-    const { lat, lon } = req.query; // Extract latitude and longitude from the query parameters
-    const weatherData = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${weatherAPI}`).then(res => res.json());
-    res.json(weatherData);
+// Weather API integration
+app.get("/weather", async (req, res) => {
+  const { lat, lon } = req.query; // Extract latitude and longitude from the query parameters
+  const weatherData = await fetch(
+    `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${weatherAPI}`,
+  ).then((res) => res.json());
+  res.json(weatherData);
 });
 
-app.get('/randomPostcode', async (req, res) => {
-    const randomPostcodeData = await fetch('http://api.postcodes.io/random/postcodes').then(res => res.json());
-    
-    const { postcode, longitude, latitude } = randomPostcodeData.result;
-    
-    // Respond with postcode, longitude, and latitude
-    res.json({ postcode, longitude, latitude });
+// Postcode API integration
+app.get("/randomPostcode", async (req, res) => {
+  const randomPostcodeData = await fetch(
+    "http://api.postcodes.io/random/postcodes",
+  ).then((res) => res.json());
+  const { postcode, longitude, latitude } = randomPostcodeData.result;
+
+  globalPostcode = postcode;
+
+  // Respond with postcode, longitude, and latitude
+  res.json({ postcode, longitude, latitude });
 });
 
-// app.get('/news', async (req, res) => {
-//     const newsData = await fetch('URL_TO_NEWS_API').then(res => res.json());
-//     res.json(newsData);
-// });
+// Open AI API Integration
+const openai = new OpenAI({
+  apiKey: openaiAPI,
+});
 
-
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     // Fetch data from API 1
-//     fetch('/api1')
-//         .then(response => response.json())
-//         .then(data => {
-//             // Update the content of the first div with data from API 1
-//             document.getElementById('apiData1').innerHTML = JSON.stringify(data);
-//         })
-//         .catch(error => console.error('Error fetching data from API 1:', error));
-
-//     // Fetch data from API 2
-//         const latitude = 51.509865; // Replace with the actual latitude
-//         const longitude = -0.118092; // Replace with the actual longitude
-
-//     // Second API
-//     fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=f4e48e65002f6d466b302a58a92f9619`)
-//         .then(response => response.json())
-//         .then(data => {
-//             // Process the historical weather data
-//             document.getElementById('apiData2').innerHTML = JSON.stringify(data);
-//         })
-//         .catch(error => console.error('Error fetching historical weather data:', error));
-// });
+app.get("/openai", async (req, res) => {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: `You are a friendly travel guide with great knowledge of uk locations. Please give a short description of the uk postcode ${globalPostcode}, talking about information of the postcode and local area. Please resond in 200 words or less.`,
+      },
+    ],
+    model: "gpt-3.5-turbo",
+    max_tokens: 300,
+  });
+  res.json(completion);
+});
